eddbe4d917b99242fcd34db35643cb1b2d7c4703
yttiiz <dominique.talis@proton.me>
2026-03-16 15:38:11 -0400

    feat(utils): create 'function' to get database & apply it in 'documents'

--- src/routes/documents.py
+++ src/routes/documents.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
+from src.utils.resources import get_database
 
 router = APIRouter(prefix="/documents", tags=["documents"])
 
+
 @router.get("/")
 def get_documents():
-    return {"document": "maco"}
+    path = get_database()
+    return {"document": path}
---
+++ src/utils/resources.py
@@ -0,0 +1,7 @@
+from src.config import settings
+
+
+def get_database():
+    path = settings.pwd_database_path
+    
+    return path
