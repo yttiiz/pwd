8b05401a5e1def234f0ac6f7c1681ba16dff4b75
yttiiz <dominique.talis@proton.me>
2026-03-18 01:15:02 -0400

    chore(database): add new exception if api key not given

--- src/routes/database.py
+++ src/routes/database.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
+
 from src.config import settings
 from src.utils.resources import get_database
 
@@ -10,6 +11,8 @@ router = APIRouter(prefix="/database", tags=["database"])
 def database(api_key: str | None = None):
     """Retreive database"""
 
+    if api_key is None:
+        raise HTTPException(status_code=403, detail="You have to provide an api key")
     if api_key != settings.api_key:
         raise HTTPException(
             status_code=403, detail="You are not authorized to get the resource."
