97abb4fdc36f5b715de0ca0b488eb66935c9e867
yttiiz <dominique.talis@proton.me>
2026-03-19 09:41:57 -0400

    chore(env): add 'app_path' needed by 'pydantic'

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     pwd_database_path: str = ""
     api_key: str = ""
+    app_path: str = ""
     origins_path: str = ""
     port: int = 8000
 
