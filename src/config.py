3e9f1d6b3687184cf09e8778bf7033ac00479dbd
yttiiz <dominique.talis@proton.me>
2026-03-20 14:05:52 -0400

    chire(config): use 'SettingsConfigDict' based on v2 pydantic improvement

--- src/config.py
+++ src/config.py
@@ -1,15 +1,15 @@
-from pydantic_settings import BaseSettings
+from typing import ClassVar
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(env_file=".env")
+
     pwd_database_path: str = ""
     api_key: str = ""
     app_path: str = ""
     origins_path: str = ""
     port: int = 8000
-
-    class Config:
-        env_file: str = ".env"
 
 
 settings = Settings()
